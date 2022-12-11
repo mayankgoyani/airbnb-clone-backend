dda74a0773fa125a64ddd47a5f3b85bfb6862421
premsharma2206 <premsharma2206@gmail.com>
2022-12-10 19:19:27 -0600

    th

--- routes/reservation.router.js
+++ routes/reservation.router.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+const passport = require("passport");
+const userAuth = passport.authenticate("jwt", { session: false });
 const reservationController = require("../controllers/reservation.controller");
 
 /* GET reservations listing. */
 router.get("/", reservationController.getAllReservation);
 router.get("/cancel/:reservationId", reservationController.cancelReservataion);
-router.post("/", reservationController.registerReservation);
+router.post("/", reservationController.addReservation);
 router.put("/", reservationController.updateReservation);
 router.get("/:reservationId", reservationController.getReservation);
 router.delete("/:reservationId", reservationController.deleteReservation);
